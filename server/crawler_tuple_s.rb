35cc022618d4ff523a59714c1ebf4f93bd4199bd
Makoto Inoue <inouemak@googlemail.com>
2011-01-25 22:43:20 +0000

    Added crawler_tuple

---
+++ server/crawler_tuple_s.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+
+
+require 'drb/drb'
+
+class ProcServer
+  def initialize(ts)
+    @ts = ts
+  end
+
+  def main_loop
+    loop do
+      tuple = @ts.take(['proc', nil])
+      request = tuple[1]
+      response = request[]
+      p response
+      @ts.write(['proc-answer', nil, response])
+    end
+  end
+end
+
+ts_uri = ARGV.shift || 'druby://localhost:12345'
+DRb.start_service
+$ts = DRbObject.new_with_uri(ts_uri)
+ProcServer.new($ts).main_loop
