8796f83a2c21093c7c6aa9e6961e2adec128f234
Makoto Inoue <inouemak@googlemail.com>
2011-01-26 10:09:56 +0000

    Took comment out

--- pattern1.rb
+++ pattern1.rb
@@ -1,33 +1,33 @@
-# class NotEqual
-#   def initialize(matcher)
-#     @matcher = matcher
-#   end
-#   def === (other)
-#     @matcher != other
-#   end
-# end
-# 
-# def NE(matcher)
-#   NotEqual.new(matcher)
-# end
-# 
-# class MatrixThree
-#   def ===(other)
-#     (other.is_a? Array) && other.length == 3  && other.all?{|e| (e.is_a? Array) && e.length == 3}
-#   end
-# end
-# 
-# three_by_three_matrix = [[1,2,3],[4,5,6],[7,8,9]]
-# 
-# 
-# [1,2,3,"foo", "bar",three_by_three_matrix].each do |example|
-#   case example
-#   when 1 then p "Number one"
-#   when String then p "String #{example}"
-#   when MatrixThree.new then p "MatrixThee: #{example.inspect}"
-#   when NE(1) then p "NotEqual #{example}"
-#   end
-# end
+class NotEqual
+  def initialize(matcher)
+    @matcher = matcher
+  end
+  def === (other)
+    @matcher != other
+  end
+end
+
+def NE(matcher)
+  NotEqual.new(matcher)
+end
+
+class MatrixThree
+  def ===(other)
+    (other.is_a? Array) && other.length == 3  && other.all?{|e| (e.is_a? Array) && e.length == 3}
+  end
+end
+
+three_by_three_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+
+[1,2,3,"foo", "bar",three_by_three_matrix].each do |example|
+  case example
+  when 1 then p "Number one"
+  when String then p "String #{example}"
+  when MatrixThree.new then p "MatrixThee: #{example.inspect}"
+  when NE(1) then p "NotEqual #{example}"
+  end
+end
 
 foo = proc{|a, b| a + b}
 p foo[1,3]
