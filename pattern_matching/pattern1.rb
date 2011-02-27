37322766cd6be28ec8c6fb5208a8a3515cfe8a94
Makoto Inoue <inouemak@googlemail.com>
2011-02-27 18:45:27 +0000

    Moved pattern matching example into its own folder

--- pattern_matching/pattern1.rb
+++ pattern_matching/pattern1.rb
@@ -20,10 +20,11 @@ end
 three_by_three_matrix = [[1,2,3],[4,5,6],[7,8,9]]
 
 
-[1,2,3,"foo", "bar",three_by_three_matrix].each do |example|
+[1,2,3,"foo", "bar",three_by_three_matrix, 100000].each do |example|
   case example
   when 1 then p "Number one"
   when String then p "String #{example}"
+  when (3..1.0/0) then p "Range (bigger than 3) #{example}"
   when MatrixThree.new then p "MatrixThee: #{example.inspect}"
   when NE(1) then p "NotEqual #{example}"
   end
