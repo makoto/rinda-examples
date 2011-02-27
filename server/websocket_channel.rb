10273c4fc37ae894f35a6da64a1330bee7e8cb5f
Makoto Inoue <inouemak@googlemail.com>
2011-02-27 11:02:02 +0000

    Added websocket_channel

---
+++ server/websocket_channel.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'em-websocket'
+require 'drb'
+require 'twitter/json_stream'
+
+username = ARGV.shift
+password = ARGV.shift
+port = ARGV.shift
+
+@channel = EM::Channel.new
+DRb.start_service("druby://localhost:#{port}", @channel)
+
+EventMachine.run{
+  @twitter = Twitter::JSONStream.connect(
+    :path => '/1/statuses/filter.json?track=a',
+    :auth => "#{username}:#{password}"
+  )
+
+  @twitter.each_item do |status|
+    @channel.push status
+  end    
+}
